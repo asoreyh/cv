e749bb42c83d3544bd449bc8dac0e07e7d17f449
asoreyh <devnull@localhost>
2012-06-14 18:54:31 +0000

    Now it makes also eng and esp short versions

--- Makefile
+++ Makefile
@@ -1,17 +1,32 @@
 all:
 	cp cv.tex cv-org.tex
+# FULL - ESP
 	xelatex cv.tex
 	xelatex cv.tex
 	mv cv.pdf cv-esp.pdf
+	cp cv-org.tex cv.tex
+# RES - ESP
+	cat cv-org.tex | sed 's/\%\\restrue/\\restrue/' > cv.tex
+	xelatex cv.tex
+	xelatex cv.tex
+	mv cv.pdf cv-res-esp.pdf
+	cp cv-org.tex cv.tex
+# FULL - ENG
 	cat cv-org.tex | sed 's/\%\\engtrue/\\engtrue/' > cv.tex
 	xelatex cv.tex
 	xelatex cv.tex
 	mv cv.pdf cv-eng.pdf
-	mv cv-org.tex cv.tex
+	cp cv-org.tex cv.tex
+# RES - ENG
+	cat cv-org.tex | sed 's/\%\\engtrue/\\engtrue/' | sed 's/\%\\restrue/\\restrue/' > cv.tex
+	xelatex cv.tex
+	xelatex cv.tex
+	mv cv.pdf cv-res-eng.pdf
+	cp cv-org.tex cv.tex
 	make clean
 
 clean:
-	rm -f *.aux *.log *.out *.bak
+	rm -f *.aux *.log *.out *.bak count.tex cv-org.tex
 
 del:
 	rm -f cv-*.pdf
---
+++ go-cv.sh
@@ -0,0 +1,39 @@
+rm count.tex
+gap=`grep -c "years" publi-gap.tex`
+cnf=`grep -c "years" publi-cnf.tex`
+ref=`grep -c "years" publi-ref.tex`
+org=`grep -c "years" orga.tex`
+div=`grep -c "years" divul.tex`
+
+echo "\ifeng
+\section*{Summary}
+\else
+\section*{Resumen}
+\fi
+
+\begin{itemize}
+" >> count.tex
+
+echo "\item \ifeng
+{\bf{${ref}}} peer review journal publications.\\
+\else
+{\bf{${ref}}} publicaciones en revistas con referato.\\
+\fi
+" >> count.tex
+
+echo "\item \ifeng
+{\bf{${cnf}}} participations and presentations at Schools \& Conferences.\\
+\else
+{\bf{${cnf}}} participaciones y presentaciones en Escuelas y Conferencias.\\
+\fi
+" >> count.tex
+
+echo "\item \ifeng
+{\bf{${gap}}} technical notes (GAP Notes) of the Pierre Auger Observatory. 
+\else
+{\bf{${gap}}} notas técnicas (GAP Notes) del Observatorio Pierre Auger. 
+\fi
+" >> count.tex
+
+echo "\end{itemize}" >> count.tex
+make
