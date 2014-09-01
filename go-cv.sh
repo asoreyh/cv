7255e797b71d24b016bc18c8a09b8bfca54159db
Hernán Asorey <asoreyh@gmail.com>
2014-09-01 13:00:17 -0500

    New version. Warning: english version could not be complete

--- go-cv.sh
+++ go-cv.sh
@@ -7,9 +7,13 @@ org=`grep -c "years" orga.tex`
 div=`grep -c "years" divul.tex`
 
 echo "\ifeng
-\section*{Summary}
+\section*{Publication summary}
+
+% As a member of the International Collaborations I belong, I participated in the publication of:
 \else
-\section*{Resumen}
+\section*{Resumen de publicaciones}
+
+% Como miembro de las instituciones a las que pertenezco, he participado en la publicación de:
 \fi
 
 \begin{itemize}
@@ -30,11 +34,25 @@ echo "\item \ifeng
 " >> count.tex
 
 echo "\item \ifeng
-{\bf{${gap}}} technical notes (GAP Notes) of the Pierre Auger Observatory. 
+ {\bf{${gap}}} technical notes (GAP Notes) of the Pierre Auger Observatory. 
 \else
 {\bf{${gap}}} notas técnicas (GAP Notes) del Observatorio Pierre Auger. 
 \fi
 " >> count.tex
 
 echo "\end{itemize}" >> count.tex
+echo '\ifeng
+See the complete list of publications, works and scitations in some of the following services:
+\else
+Ver la lista completa de publicaciones, trabajos y citaciones en alguno de los siguientes servicios:
+\fi
+
+\begin{description}
+\item[ORCID]: \href{http://orcid.org/0000-0002-4559-8785}{orcid.org/0000-0002-4559-8785}
+\item[Google Scholar]: \href{http://scholar.google.com.co/citations?user=Vj7_fGsAAAAJ}{scholar.google.com.co/citations?user=Vj7\_fGsAAAAJ}
+\item[Scopus]: \href{http://www.scopus.com/authid/detail.url?authorId=35276880300}{www.scopus.com/authid/detail.url?authorId=35276880300}
+\item[Inspire-HEP]: \href{http://inspirehep.net/author/profile/H.Asorey.1}{inspirehep.net/author/profile/H.Asorey.1}
+\item[COLCIENCIAS]: \href{http://190.216.132.131:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000005467}{190.216.132.131:8081/cvlac/visualizador/generarCurriculoCv.do?cod\_rh=0000005467}
+\end{description}
+' >> count.tex
 make
